c52020d4e9afba3c45ff4eac1f71a1285c8ba9bc
Florent Thiéry <florent.thiery@ubicast.eu>
2019-09-12 16:38:11 +0200

    avoid starting another ssh tunnel during teardown which may freeze the subprocess

--- mm_client/lib/ssh_tunnel.py
+++ mm_client/lib/ssh_tunnel.py
@@ -104,12 +104,13 @@ class SSHTunnelManager():
                 target = target[:-1]
             self.update_ssh_state('command', prepare_ssh_command(target, self.ssh_tunnel_state['port']))
             logger.info('Starting SSH with command:\n    %s', ' '.join(self.ssh_tunnel_state['command']))
-            self.process = subprocess.Popen(self.ssh_tunnel_state['command'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
-            self.stdout_queue = multiprocessing.Queue()
-            self.stdout_reader = AsynchronousFileReader(self.process.stdout, self.stdout_queue)
-            self.stdout_reader.start()
-            self.stderr_reader = AsynchronousFileReader(self.process.stderr, self.stdout_queue)
-            self.stderr_reader.start()
+            if self.loop_ssh_tunnel:
+                self.process = subprocess.Popen(self.ssh_tunnel_state['command'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
+                self.stdout_queue = multiprocessing.Queue()
+                self.stdout_reader = AsynchronousFileReader(self.process.stdout, self.stdout_queue)
+                self.stdout_reader.start()
+                self.stderr_reader = AsynchronousFileReader(self.process.stderr, self.stdout_queue)
+                self.stderr_reader.start()
         else:
             logger.debug('No port provided, not starting ssh tunnel')
 
